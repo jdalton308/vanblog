cad2b6f048e209bfb43bce308cf14095d45caeae
jdalton308 <jdalton308@yahoo.com>
2016-06-17 16:17:20 -0700

    IG photos are working!

--- build/server-instagram.js
+++ build/server-instagram.js
@@ -1,121 +1,27 @@
 'use strict';
 
-const IG_id = '9e8f810b45034d1d973cc0f825af5d61';
-const IG_secret = '84d4b1ca4014496cb2ec0ed14e12fa36';
+// const IG_id = '9e8f810b45034d1d973cc0f825af5d61';
+// const IG_secret = '84d4b1ca4014496cb2ec0ed14e12fa36';
 const access_token = '678278338.1677ed0.ee369683d6c54a7f8260dbb7e9271ef2';
-const post_count = 15;
-const redirect_uri = 'www.joeplusvan.com';
+// const post_count = 15;
+// const redirect_uri = 'www.joeplusvan.com';
 
 
 const http = require('http');
-// const ig = require('instagram-node').instagram();
 let instagramAPI = require('instagram-api');
 instagramAPI = new instagramAPI(access_token);
 
 
-// ig.use({ client_id: IG_id, client_secret: IG_secret });
-
-
-// STEPS:
-// - On client request for '/instagram',
-// - If no access_token, get token, save it, then,
-// - Make call for photo data
-// - Finally, return photo data
-
-
-// Get access token. Returns a promise
-// function getAccessToken(req) {
-
-// 	let promise = new Promise( (resolve, reject) => {
-// 		console.log('Inside promise...');
-// 		ig.authorize_user(req.query.code, redirect_uri, (err, result) => {
-// 			if (err) {
-// 				console.log('Error retrieving access_token:')
-// 				console.log(err.body);
-// 				reject(err);
-// 			} else {
-// 				console.log('Recieved access token:');
-// 				console.log(result);
-
-// 				access_token = result.access_token;
-// 				resolve(result);
-// 			}
-// 		});
-// 	});
-
-// 	return promise;
-// }
-
 // Get photos. Returns a promise
 function getPhotoData(res1) {
-
-	// let request_URL = 'https://api.instagram.com/v1/users/self/media/recent/?access_token=' + access_token + '&count=' + post_count;
-
-	// let promise = new Promise( (resolve, reject) => {
-		// console.log('Request for '+ request_URL);
-		// let options = {
-		// 	count: post_count
-		// }
-
-		// console.log('getting data...');
-
-		// ig.user_self_feed(options, (err, medias, pagination, remaining, limit) => {
-		// 	let data = {
-		// 		err: err,
-		// 		medias: medias,
-		// 		pagination: pagination,
-		// 		remaining: remaining,
-		// 		limit: limit
-		// 	};
-		// 	resolve(data);
-		// });
-
-
-		// let request = http.get(request_URL, (res) => {
-		// 	res.setEncoding('utf8');
-		// 	console.log('IG Response:');
-		// 	console.log(res);
-
-		// 	let photoData = '';
-
-		// 	// res.on('data', (data) => {
-		// 		// console.log('Recieved some IG data...');
-		// 		// photoData += data;
-		// 	// });
-		// 	// res.on('end', () => {
-		// 	console.log('Finished recieving IG data');
-
-		// 	// RESOLVE PROMISE
-		// 	resolve(res);
-
-		// 	res.resume();
-		// 	// });
-		// });
-
-		// request.on('error', (err) => {
-		// 	console.error('Error getting user Instagram data');
-		// 	console.error(err);
-		// 	reject(err);
-		// });
-
-		// request.on('timeout', (err) => {
-		// 	console.log('IG request timeout');
-		// 	req.abort();
-		// 	reject(err);
-		// });
-
-		// request.setTimeout(10000);
-
-		
-	// });
-
-	// promise.then(
 	instagramAPI.userSelfMedia().then(
 		(data) => {
 			console.log('Recieved IG data...');
 			res1.writeHead(200, {'Content-Type': 'application/json'});
-			let stringData = data.toString('utf8');
-			res1.end(stringData);
+			// console.log(data);
+			// let stringData = data.toString('utf8');
+			let jsonData = JSON.stringify(data);
+			res1.end(jsonData);
 		}
 	).catch(
 		(error) => {
@@ -127,30 +33,5 @@ function getPhotoData(res1) {
 
 }
 
-// Main Logic
-function getPhotos(res) {
 
-	// If have access_token, just get photos:
-	// if (access_token) {
-		getPhotoData(res);
-
-	// If no access_token...
-	// } else {
-
-	// 	// Get access token, then photo data...
-	// 	getAccessToken(req).then(
-	// 		(data) => {
-	// 			return getPhotoData(res);
-	// 		}
-	// 	).catch(
-	// 		(err) => {
-	// 			res.writeHead(500);
-	// 			return res.end('Could not get access token');
-	// 		}
-	// 	);
-
-	// }
-}
-
-
-module.exports = getPhotos;
+module.exports = getPhotoData;
--- src/js/modules/connect.js
+++ src/js/modules/connect.js
@@ -8,12 +8,25 @@ function getIGphotos() {
 	var $photoCont = $('.ig-post-list');
 
 	// $.get(ig_request_url)
-	$.get({
+	$.ajax({
+		type: 'GET',
 		url: ig_request_url,
 		dataType: 'json'
 	}).done(function(data){
 			console.log('Recieved IG data:');
 			console.log(data);
+
+			// Posts = data.data array
+			data.data.forEach(function(post, i){
+				var $imgLink = $('<a href="' + post.link + '" target="_blank" class="ig-link"></a>')
+				var imgHTML = '<img src="' + post.images.standard_resolution.url + '"">';
+
+				// Create HTML
+				$imgLink.html(imgHTML);
+
+				// Add to page
+				$photoCont.append($imgLink);
+			});
 		})
 		.fail(function(err){
 			console.log('Error getting IG data:');
