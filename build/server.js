c02aa2aa77ccff6aae384284c5f0f7bf583fc38b
jdalton308 <jdalton308@yahoo.com>
2016-07-08 13:07:56 -0700

    Saved file and built

--- build/server.js
+++ build/server.js
@@ -9,12 +9,6 @@ const server = http.createServer((req,res) => {
 	router(req, res);
 });
 
-<<<<<<< HEAD
 server.listen(80);
 
 console.log("Server started on port 80");
-=======
-server.listen(3000, 'localhost');
-
-console.log('Server started...');
->>>>>>> master
