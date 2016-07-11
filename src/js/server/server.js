edf856b8f5e159b6309921d7d6111e38e226e54b
jdalton308 <jdalton308@yahoo.com>
2016-07-11 11:22:18 -0700

    Added host address to server

--- src/js/server/server.js
+++ src/js/server/server.js
@@ -2,6 +2,7 @@
 
 const http = require('http');
 const router = require('./server-router.js');
+const EC2_IP = '52.11.115.75';
 
 
 // Create Server
@@ -9,6 +10,6 @@ const server = http.createServer((req,res) => {
 	router(req, res);
 });
 
-server.listen(80);
+server.listen(80, EC2_IP);
 
-console.log("Server started on port 80");
+console.log("Server started on port 80 for "+ EC2_IP);
